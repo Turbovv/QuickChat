1e68b66fa9565800a3349086ea51440e68842fda
Turbovv <108888834+Turbovv@users.noreply.github.com>
2023-10-31 18:43:23 +0400

    fix: add users

--- server/app.js
+++ server/app.js
@@ -1,30 +1,41 @@
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
+const users = require('./users')()
 
 const m = (name, text, id) => ({name, text, id})
 
 io.on('connection', socket => {
-
   socket.on('userJoined', (data, cb) => {
     if (!data.name || !data.room) {
       return cb('Warning')
     }
 
     socket.join(data.room)
+
+    users.remove(socket.id)
+    users.add({
+      id: socket.id,
+      name: data.name,
+      room: data.room
+    })
+
     cb({userId: socket.id})
     socket.emit('newMessage', m('admin', `Welcome ${data.name}`))
-    socket.emit('newMessage', m('TEST', `Welcome`))
     socket.broadcast.to(data.room)
       .emit('newMessage', m('admin', `${data.name} Joined`))
   })
 
-  socket.on('createMessage', data => {
-    setTimeout(() => {
-        socket.emit('newMessage', {
-          text: data.text + 'SERVER'
-        })
-    }, 500)
+  socket.on('createMessage', (data, cb) => {
+    if (!data.text) {
+      return cb('Text is required')
+    }
+
+    const user = users.get(data.id)
+    if (user) {
+      io.to(user.room).emit('newMessage', m(user.name, data.text, data.id))
+    }
+    cb()
   })
 })
 
